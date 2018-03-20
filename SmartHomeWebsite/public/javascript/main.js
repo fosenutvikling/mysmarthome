f7ecc272bb5e17aef4e9dec75a17613efeda69c1
Kristian Sundhaugen <kristiansundhaugen@gmail.com>
2018-03-20 15:41:05 +0100

    Added some code for showing time and date

--- SmartHomeWebsite/public/javascript/main.js
+++ SmartHomeWebsite/public/javascript/main.js
@@ -3,33 +3,36 @@
 // Increment button function for set temperature
 
 $(document).ready(function(){
-        const MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
+    const MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
-        const DAY_NAMES = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-            "Saturday", "Sunday"
-        ];
+    const DAY_NAMES = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+        "Saturday", "Sunday"
+    ];
 
 
-        var today = new Date();
-        var day = today.getDay();
-        document.getElementById('dayOf').innerHTML = DAY_NAMES[day -1];
+    var today = new Date();
+    var day = today.getDay();
+    document.getElementById('dayOf').innerHTML = DAY_NAMES[day -1];
 
-        var month = today.getMonth();
-        var year = today.getFullYear();
-         document.getElementById('dateOf').innerHTML =
-             MONTH_NAMES[month] + " " + year;
+    var month = today.getMonth();
+    var year = today.getFullYear();
+    document.getElementById('dateOf').innerHTML =
+        MONTH_NAMES[month] + " " + year;
+    console.log(month);
 
-        var hour = today.getHours();
-        var minute = today.getMinutes();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    checkTime(minute);
+    document.getElementById('timeOf').innerHTML =
+        hour + ":" + minute;
 
-        document.getElementById('timeOf').innerHTML =
-            hour + ":" + checkTime(minute);;
     function checkTime(i) {
         if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
         return i;
     }
+
 
     var quantitiy=0;
     $('.turnUp').click(function(e){
