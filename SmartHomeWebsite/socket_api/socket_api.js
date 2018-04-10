5f70024b9e2f75aedea688af4b9363bd22e32cdd
martin pukstad <martinpu@stud.ntnu.no>
2018-04-10 15:54:30 +0200

    added new channel for humidity to fix issue with duplication on emits.

--- SmartHomeWebsite/socket_api/socket_api.js
+++ SmartHomeWebsite/socket_api/socket_api.js
@@ -4,28 +4,24 @@ var socketApi = {};
 var db = require("../model/connection");
 socketApi.io = io;
 
-io.on('connection', function (socket){ // Socket connection
+io.on('connect', function (socket){ // Socket connection
+	var sessionid = socket.id; // Store the ID of the connected client.
+	console.log(sessionid);
    console.log('connection'); // If connection successful, print.
+
    socket.emit("CH01", "Connecting to the server"); // Trigger message to start the listener on the client side.
+   socket.emit("CH02", "Channel 2"); // Trigger for channel 2 humidity
+
   socket.on('CH01', function (from, msg) { // Listen on channel CH01 and write what is sent from the client.
-    if (msg.Topic === "temp") { // If topic is temperature
     console.log('MSG', from, msg);
-    db.sensor_query(msg.SensorID.toString(), msg.Data.toString(), msg.RaspberryID.toString());
-    socket.emit('CH01', 'SERVER: Message Recieved with the Topic: ' + msg.Topic); // msg is a JSON object, .Topic, .Date and .Data is available.
-
-    //socket.disconnect(true);
-
-    } else if(msg.Topic === "humm") {
-        if (msg.Data === null)
-          console.log("No data available");
-      console.log("MSG", from, msg);
-      db.sensor_query(msg.SensorID.toString(), msg.Data.toString(), msg.RaspberryID.toString());
-      socket.emit("CH01", "SERVER: Message Recieved with the Topic: " + msg.Topic);
-    } else {
-      console.log("No known topic given");
-    }
+    db.sensor_query(msg.SensorID.toString(), msg.Data.toString(), msg.RaspberryID.toString(), msg.Topic.toString());
+    socket.emit('CH01', msg.Topic); // msg is a JSON object, .Topic, .Date and .Data is available.
   });
-
+  socket.on('CH02', function (from, msg) { // Listen on channel CH02 and write what is sent from the client.
+    console.log("MSG", from, msg);
+    db.sensor_query(msg.SensorID.toString(), msg.Data.toString(), msg.RaspberryID.toString(), msg.Topic.toString());
+    socket.emit("CH02", msg.Topic);
+  });
 });
 
 
